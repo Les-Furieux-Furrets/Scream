c78af1b1b5ebb5e6e3450e9aa3cfb877632d97a1
nicolas-gourdon <nic7_@hotmail.fr>
2015-11-02 18:02:38 +0100

    Zoom

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -5,17 +5,31 @@ public class Follow : MonoBehaviour
 {
 	public Transform master;
 	Vector3 camPos;
+	KeyCode Dezoom = KeyCode.W;
+	float x;
+	float y;
 
 	void Start () 
 	{
-	
+		x = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		camPos = master.position;
-		camPos.y = 95;
+
+		//transform.position = camPos;
+		if (Input.GetKey (Dezoom)) {
+			camPos.y=95;
+			camPos.y = Mathf.MoveTowards (camPos.y,300,x);
+			x = x+ (Time.deltaTime * 100);
+			y = 0;
+		} else {
+			x=0;
+			camPos.y = 95;
+		}
 		transform.position = camPos;
 	}
-}
+	}
+
